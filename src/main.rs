3abf392d1ad6cd04be6916bca1cab73b596e006a
iZoom786 <asifparacha786@gmail.com>
2026-01-18 18:53:25 +0000

    ok

--- src/main.rs
+++ src/main.rs
@@ -16,14 +16,20 @@ async fn add(query: web::Query<AddQuery>) -> Result<String> {
     let sum = query.a + query.b;
     Ok(format!("The sum of {} and {} is {}", query.a, query.b, sum))
 }
+
+async fn index() -> Result<String> {
+    Ok("Rust API is running. Use /add?a=1&b=2 to add numbers.".to_string())
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    println!("Starting server on http://0.0.0.0:8080");
+    println!("Starting server on http://127.0.0.1:8080");
     HttpServer::new(|| {
         App::new()
+            .route("/", web::get().to(index))
             .route("/add", web::get().to(add))
     })
-    .bind("0.0.0.0:8080")?
+    .bind("127.0.0.1:8080")?
     .run()
     .await
 }
