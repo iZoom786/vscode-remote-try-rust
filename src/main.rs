195d4ef14cfb8edf3ee3e7bebd69276171e8d4b5
iZoom786 <asifparacha786@gmail.com>
2026-01-18 18:46:33 +0000

    all ok

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,6 @@ async fn add(query: web::Query<AddQuery>) -> Result<String> {
     let sum = query.a + query.b;
     Ok(format!("The sum of {} and {} is {}", query.a, query.b, sum))
 }
-
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     println!("Starting server on http://0.0.0.0:8080");
